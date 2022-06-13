b29ca1b4dca41788e788db8bdcf7ea4a66029d00
Sharon1302 <52819755+Sharon1302@users.noreply.github.com>
2022-06-13 08:01:46 +0100

    Add files via upload

--- src/components/slider/slider-data.js
+++ src/components/slider/slider-data.js
@@ -1,19 +1,19 @@
 export const sliderData = [
     {
-       image:"https://www.josla.com.ng/static/media/electricCharge.0df93834.png",
+       image:"/electricCharge.0df93834.png",
         heading1:"Electricty as a Service",
         
         desc: "With our Smart Meter, we want to contribute as a local company that leverages information technology to match supply and demand of electricity in real-time.",
      },
     
     {
-      image:"https://www.josla.com.ng/static/media/city.6079248a.png",
+      image:"/city.6079248a.png",
       heading1:"Building Nigeria's Smart grid",
       
        desc: "With our Smart Meter, we want to contribute as a local company that leverages information technology to match supply and demand of electricity in real-time.",
     },
     {
-      image:"https://www.josla.com.ng/static/media/billUser.e6c6ac90.png",
+      image:"/billUser.e6c6ac90.png",
       heading1:"Pay Electricity Bills ",
         
         
